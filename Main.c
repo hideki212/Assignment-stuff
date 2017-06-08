840b0747b1442259cde5bb7f1b99e79c16241483
hideki212 <dekiyana@gmail.com>
2017-06-08 19:22:34 +0800

    Fixed calendar issue
    Calendar issue printing 1 day late bug fix

--- Main.c
+++ Main.c
@@ -1,43 +1,29 @@
 #include <stdio.h>
 
-char *months[] =
-    {
-        " ",
-        "\nJanuary",
-        "\nFebruary",
-        "\nMarch",
-        "\nApril",
-        "\nMay",
-        "\nJune",
-        "\nJuly",
-        "\nAugust",
-        "\nSeptember",
-        "\nOctober",
-        "\nNovember",
-        "\nDecember"};
-
-int month_days[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-void calender();
 void calc();
 void prime();
 void oddEven();
 void minMax();
-void buildCalender(int month, int year, int first_day);
-int leapyear(int year);
-int first_day_year(int year);
-int checkPrimeNumber(int number);
 void addFunc(float x, float y);
 void subFunc(float x, float y);
 void multiplyFunc(float x, float y);
 void divideFunc(float x, float y);
-float num(int x);
 void sym(float x, float y);
 void getEven(int number);
 void getOdd(int number);
 void getInput();
 void findMin();
 void findMax();
+void calendar(int y, int m);
+void startCalendar();
+
+int checkPrimeNumber(int number);
+int todayOf(int y, int m, int d);
+int leapYears(int y);
+int isLeapYear(int y);
+
+long days(int y, int m, int d);
+float num(int x);
 
 int numbers[100];
 
@@ -56,7 +42,7 @@ int main()
         switch (choice)
         {
         case 1:
-            calender();
+            startCalendar();
             break;
 
         case 2:
@@ -87,168 +73,175 @@ int main()
     }
 }
 
-int first_day_year(int year)
+/*
+    True if leap year
+    child method of calender
+*/
+
+int isLeapYear(int y)
 {
-    int first_day;
-    int x;
-    int y;
-    int z;
-
-    x = (year - 1.) / 4.0;
-    y = (year - 1.) / 100.;
-    z = (year - 1.) / 400.;
-
-    first_day = (year + x - y + z) % 7;
-
-    return first_day;
+    return (y % 400 == 0) || ((y % 4 == 0) && (y % 100 != 0));
 }
 
-int leapyear(int year)
+/*
+    The number of leap year
+    child method of calender
+*/
+int leapYears(int y)
 {
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
-    {
-        month_days[2] = 29;
-        return 1;
-    }
-    else
-    {
-        month_days[2] = 28;
-        return 0;
-    }
+    return y / 4 - y / 100 + y / 400;
 }
 
-void buildCalender(int month, int year, int first_day)
+/*
+    number of days since the beginning of the year
+    child method of calender
+*/
+int todayOf(int y, int m, int d)
 {
-    int i;
+    static int DayOfMonth[] =
+        {-1 /*dummy*/, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
 
-    printf("%s %d\n\n", months[month], year);
-    printf("Sun  Mon  Tue  Wed  Thu  Fri  Sat\n");
-
-    for (i = 0; i < month; i++)
-    {
-        first_day = (first_day + month_days[month]) % 7;
-    }
-
-    for (i = 1; i <= 1 + first_day * 5; i++)
-    {
-        printf(" ");
-    }
-
-    for (i = 1; i <= month_days[month]; i++)
-    {
-        printf("%2d", i);
-
-        if ((i + first_day) % 7 > 0)
-            printf("   ");
-        else
-            printf("\n ");
-    }
+    return DayOfMonth[m] + d + ((m > 2 && isLeapYear(y)) ? 1 : 0);
 }
-
-void calender()
+/*
+    Total number of days
+    child method of calender
+*/
+long days(int y, int m, int d) /* Total number of days */
 {
-    int year;
-    int month;
-    int first_day;
+    int lastYear;
 
-    do
+    lastYear = y - 1;
+
+    return 365L * lastYear + leapYears(lastYear) + todayOf(y, m, d);
+}
+/*
+    Start calender functionality here
+    has 1 child method called Calender()
+*/
+void startCalendar()
+{
+    int year, month;
+
+    printf("Enter the month and year: ");
+    scanf("%d %d", &month, &year);
+
+    calendar(year, month);
+}
+/*
+    display calendar at m y
+    child method of startCalender
+*/
+void calendar(int y, int m) 
+{
+    const char *NameOfMonth[] = {NULL /*dummp*/,
+                                 "January", "February", "March", "April", "May", "June",
+                                 "July", "August", "September", "October", "November", "December"};
+    char Week[] = "Su Mo Tu We Th Fr Sa";
+    int DayOfMonth[] =
+        {-1 /*dummy*/, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int weekOfTopDay;
+    int i, day;
+
+    weekOfTopDay = days(y, m, 1) % 7;
+    if (isLeapYear(y))
+        DayOfMonth[2] = 29;
+    printf("\n     %s %d\n%s\n", NameOfMonth[m], y, Week);
+
+    for (i = 0; i < weekOfTopDay; i++)
+        printf("   ");
+    for (i = weekOfTopDay, day = 1; day <= DayOfMonth[m]; i++, day++)
     {
-        printf("Enter a month (1 - 12): ");
-        scanf("%d", &month);
-    } while (month < 1 || month > 12);
-
-    do
-    {
-        printf("Enter a year (1978 - 3000): ");
-        scanf("%d", &year);
-    } while (year < 1978 || year > 3000);
-
-    first_day = first_day_year(year);
-    leapyear(year);
-    buildCalender(month, year, first_day);
-    printf("\n\n\n");
+        printf("%2d ", day);
+        if (i % 7 == 6)
+            printf("\n");
+    }
+    printf("\n");
 }
 
 //Calculator and functions
 void calc()
 {
-	float num1 = num(1);
-	float num2 = num(2);
-	sym(num1, num2);
-	printf("This calculator will now shut down. If you have any complaints please direct them to /dev/null.\n");
+    float num1 = num(1);
+    float num2 = num(2);
+    sym(num1, num2);
+    printf("This calculator will now shut down. If you have any complaints please direct them to /dev/null.\n");
 }
 
 void addFunc(float x, float y) // +
 {
-	printf("The total is %.2f.\n", x + y);
+    printf("The total is %.2f.\n", x + y);
 }
 
 void subFunc(float x, float y) // -
 {
-	printf("The total is %.2f.\n", x - y);
+    printf("The total is %.2f.\n", x - y);
 }
 
 void multiplyFunc(float x, float y) // *
 {
-	printf("The total is %.2f.\n", x * y);
+    printf("The total is %.2f.\n", x * y);
 }
 
 void divideFunc(float x, float y) // /
 {
-	printf("The total is %.2f.\n", x / y);
+    printf("The total is %.2f.\n", x / y);
 }
 
 float num(int x) //Number input
 {
-	float y;
-	int valid;
-	do 
-	{
-		printf("Enter number %d: ", x);
-		scanf(" %f", &y);
-		if (x == 0) //data validation
-			printf("What are you trying to do, divide by zero? Why do you even want to use zero anyways?\n");
-		else if (x == '+' || x == '-' || x == '/' || x == '*')
-			printf("Why are you even entering a symbol here? Numbers only.\n");
-		else
-			valid = 1;
-	} while (valid != 1);
-	return y;
+    float y;
+    int valid;
+    do
+    {
+        printf("Enter number %d: ", x);
+        scanf(" %f", &y);
+        if (x == 0) //data validation
+            printf("What are you trying to do, divide by zero? Why do you even want to use zero anyways?\n");
+        else if (x == '+' || x == '-' || x == '/' || x == '*')
+            printf("Why are you even entering a symbol here? Numbers only.\n");
+        else
+            valid = 1;
+    } while (valid != 1);
+    return y;
 }
 
 void sym(float x, float y) //Symbol input and answer display
 {
-	char z;
-	int valid = 0;
-	do
-	{
-		printf("Enter a symbol: ");
-		scanf(" %c", &z);
-		if (z == '+') 
-		{
-			addFunc(x, y);
-			valid = 1;
-		}
-		else if (z == '-')
-		{
-			subFunc(x, y);
-			valid = 1;
-		}
-		else if (z == '*')
-		{
-			multiplyFunc(x, y);
-			valid = 1;
-		}
-		else if (z == '/')
-		{
-			divideFunc(x, y);
-			valid = 1;
-		}
-		else
-			printf("Support for other symbols will come never. For now please use the +, -, *, and / symbols.\n");
-	} while (valid != 1);
+    char z;
+    int valid = 0;
+    do
+    {
+        printf("Enter a symbol: ");
+        scanf(" %c", &z);
+        if (z == '+')
+        {
+            addFunc(x, y);
+            valid = 1;
+        }
+        else if (z == '-')
+        {
+            subFunc(x, y);
+            valid = 1;
+        }
+        else if (z == '*')
+        {
+            multiplyFunc(x, y);
+            valid = 1;
+        }
+        else if (z == '/')
+        {
+            divideFunc(x, y);
+            valid = 1;
+        }
+        else
+            printf("Support for other symbols will come never. For now please use the +, -, *, and / symbols.\n");
+    } while (valid != 1);
 }
-
+/*
+    Get prime number
+    has 2 child methods
+*/
 void prime()
 {
     int number1, number2, i, found;
@@ -268,7 +261,10 @@ void prime()
 
     printf("\n");
 }
-
+/*
+    Check for prime number
+    Child method to prime()
+*/
 int checkPrimeNumber(int number)
 {
     int j, found = 1;
@@ -284,7 +280,9 @@ int checkPrimeNumber(int number)
 
     return found;
 }
-
+/*
+    Input odd even numbers
+*/
 void oddEven()
 {
     int number1, number2, i, found;
@@ -292,7 +290,7 @@ void oddEven()
     printf("Enter two positive integers: ");
     scanf("%d %d", &number1, &number2);
 
-    printf("Prime numbers in range %d and %d are:\n", number1, number2);
+    printf("Odd even in range %d and %d are:\n", number1, number2);
 
     for (i = number1 + 1; i < number2; ++i)
     {
@@ -306,17 +304,23 @@ void oddEven()
 
     printf("\n");
 }
-
+/*
+    Find odd number function
+*/
 void getOdd(int number)
 {
     printf("%d is odd.", number);
 }
-
+/*
+    Find Even number function
+*/
 void getEven(int number)
 {
     printf("%d is even.", number);
 }
-
+/*
+    Input Odd Even
+*/
 void minMax()
 {
     printf("How many number do you want to comapare: ");
@@ -329,8 +333,13 @@ void minMax()
     {
         scanf("%d", &numbers[i]);
     }
+    findMin();
+    findMax();
 }
-
+/*
+    Find Min
+    child method of minMax()
+*/
 void findMin()
 {
     values = numbers[0];
@@ -345,7 +354,10 @@ void findMin()
 
     printf("Minimum = %d \n", values);
 }
-
+/*
+    Find Max
+    child method of minMax()
+*/
 void findMax()
 {
     values = numbers[0];
@@ -359,4 +371,3 @@ void findMax()
     }
     printf("Maximum = %d\n", values);
 }
-
